6eede9d2d7d786312a6cbd80970e1a368cae70e0
Stefano Mancini <stefano.mancini@devinterface.com>
2022-01-25 16:22:58 +0100

    from query string to bearer

--- middlewares/api_token_auth.middleware.go
+++ middlewares/api_token_auth.middleware.go
@@ -1,6 +1,8 @@
 package middlewares
 
 import (
+	"strings"
+
 	"devinterface.com/startersaas-go-api/services"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/bson"
@@ -9,14 +11,20 @@ import (
 
 var accountService = services.AccountService{}
 
-// AuthReq middleware
+// APITokenAuth middleware
 var APITokenAuth = func(ctx *fiber.Ctx) (err error) {
-	apiToken := ctx.Query("apiToken")
-	if apiToken != "" {
-		account, _ := accountService.OneBy(bson.M{"apiToken": apiToken})
-		if account.ID != primitive.NilObjectID {
-			ctx.Locals("currentAccount", account)
-			return ctx.Next()
+	bearer := ctx.Get("authorization")
+	if len(bearer) > 0 {
+		components := strings.SplitN(bearer, " ", 2)
+		if len(components) == 2 {
+			apiToken := components[1]
+			if apiToken != "" {
+				account, _ := accountService.OneBy(bson.M{"apiToken": apiToken})
+				if account.ID != primitive.NilObjectID {
+					ctx.Locals("currentAccount", account)
+					return ctx.Next()
+				}
+			}
 		}
 	}
 	return ctx.Status(401).JSON(fiber.Map{
