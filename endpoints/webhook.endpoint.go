c80662e12acc7c0ba53bb174d57b32e6a1c8b41c
Stefano Mancini <stefano.mancini@devinterface.com>
2023-01-23 11:01:38 +0100

    removed unused service

--- endpoints/webhook.endpoint.go
+++ endpoints/webhook.endpoint.go
@@ -3,7 +3,6 @@ package endpoints
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/stripe/stripe-go/v72"
-	"go.mongodb.org/mongo-driver/bson"
 )
 
 // WebhookEndpoint struct
@@ -16,14 +15,6 @@ func (webhookEndpoint *WebhookEndpoint) HandleWebhook(ctx *fiber.Ctx) error {
 	payload := make(map[string]interface{})
 	ctx.BodyParser(&payload)
 	go webhookService.HandleWebhook(payload, event)
-	return ctx.JSON(fiber.Map{
-		"success": true,
-	})
-}
-
-func (webhookEndpoint *WebhookEndpoint) Fattura24(ctx *fiber.Ctx) error {
-	account, _ := accountService.OneBy(bson.M{"subdomain": "devinterface"})
-	fattura24Service.DummyGenerateInvoice(account.ID)
 	return ctx.JSON(fiber.Map{
 		"success": true,
 	})
